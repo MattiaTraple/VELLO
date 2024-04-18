07f8b0516a125a9dc33804655d9132702b62104b
Mattia <matti.rosso.mt@gmail.com>
2024-04-18 22:23:32 +0200

    implementazione funzioante della personalità, riscontro ancora un problema a fare le fun in un file separato, epr ora sono assieme a random_generator.py

--- social/func/random_generator.py
+++ social/func/random_generator.py
@@ -2,7 +2,7 @@ import os
 import random
 import json
 
-from personality_managment import big_five_generator
+#from personality import big_five_generator
 
 # Range di età che vengono usati, con le relative probabilità
 range_age = [(range(13, 17), 0.066), (range(17, 24), 0.171), (range(25, 34), 0.385), (range(35, 49), 0.207), (range(50, 102), 0.171)]
@@ -70,3 +70,45 @@ def content_interaction_gen_prob(prob):
 def personality_manager():
     # Al momento le personalità sono molto estreme, potrei usare una gaussiana per mitigare i valori e tenerli il più possibile nella media
     return
+
+
+
+# Dict che viene poi salvato nel rispettivo attributo del agente
+big_five = {}
+
+# Fun per generazione dei 5 valori per i 5 big five di ogni agents
+def big_five_generator():
+    deviazione_standard = 0.2
+    
+    # Generazione iniziale dei valori 
+    for trait in ["apertura mentale", "coscienziosità", "estroversione", "gradevolezza", "nevroticismo"]:
+        value = round(random.gauss(0.5, deviazione_standard), 2)
+        value = max(min(value, 1), 0.01)  # Limitiamo i valori nell'intervallo da 0.01 a 1
+        big_five[trait] = value
+    
+    balance_function()
+   
+    return big_five
+
+# Fun aus per eseguire opportuni bilanciamenti agli indici estratti
+def balance_function():
+    # Vero e prorpio bilanciamento
+    # Bilancio la coppia e-g
+    big_five["estroversione"],big_five["gradevolezza"]=couple_balancer(big_five["estroversione"],big_five["gradevolezza"])
+    # Bilancio la coppia n-c
+    big_five["nevroticismo"],big_five["estroversione"]=couple_balancer(big_five["nevroticismo"],big_five["estroversione"])
+    # Bilancio la coppia c-g
+    big_five["coscienziosità"],big_five["gradevolezza"]=couple_balancer(big_five["coscienziosità"],big_five["gradevolezza"])
+
+
+# Fun aus a cui passo due campi e li bilnacio
+def couple_balancer(big_1, big_2):
+    # Differenza tra i campi dopo la quale viene iniziato il bilanciaento
+    soglia=0.3
+    # Se il || della differenza è sopra la soglia, quindi troppa differneza
+    if abs(big_1-big_2) > soglia:
+        if big_1>big_2:
+            big_2+=0.1
+        else:
+            big_1+=0.1
+    return round(big_1,2),round(big_2,2)
